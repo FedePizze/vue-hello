113f0d26bd81e462067f0cd4e57b4fcd816e0287
Federico Pizzeghello <pizzeghello.work@gmail.com>
2022-02-09 15:54:09 +0100

    script completato

---
+++ script.js
@@ -0,0 +1,10 @@
+const root = new Vue (
+    {
+        el : "#root",
+        data: {
+            messaggio: "ciao a tutti",
+            immaggine: "https://picsum.photos/200/300",
+        }
+
+    }
+);
